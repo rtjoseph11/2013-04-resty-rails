204a3f9f74be5bce7c11c8e5056538ddab65a085
Hack Reactor Students <github@hackreactor.com>
2013-05-31 15:58:34 -0700

    handling put and get requests for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,19 @@
 class CommentsController < ApplicationController
+  respond_to :html, :json, :js
   def index
   end
 
   # GET /comments/1
   def show
+    @comment = Comment.find params['id']
+    render :json => @comment
   end
 
 
   # POST /comments
   def create
-    newComment = Comment.new params['comment']
-    if newComment.save
+    @comment = Comment.new params['comment']
+    if @comment.save
       render :nothing => true, :status => 201
     else
       render :nothing => true, :status => 400
@@ -19,6 +22,12 @@ class CommentsController < ApplicationController
 
   # PUT /comments/1
   def update
+    @comment = Comment.find params['id']
+    if @comment.update_attributes params['comment']
+      render :nothing => true, :status => 204
+    else
+      render :nothing => true, :status => 400
+    end
   end
 
   # DELETE /comments/1
